c06eaadcacde2d7706a50f7ae14bedb82bda47a8
nuzhamusyafira <32903481+nuzhamusyafira@users.noreply.github.com>
2019-02-12 11:04:52 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,32 +1,56 @@
 import math
 import numpy as np
-csv2 = np.genfromtxt('pima.csv', usecols=(-1), delimiter=",")
-csv = np.genfromtxt('pima.csv', usecols=range(8), delimiter=",")
-n=len(csv)
+# we extract the last column as it appears to be the class and name it as y_train,
+# y_train indicates what our predicted class will be compared to
+y_train = np.genfromtxt('pima.csv', usecols=(-1), delimiter=",")
+
+# then we define the rest, which are predictors, as x_train
+x_train = np.genfromtxt('pima.csv', usecols=range(8), delimiter=",")
+
+# we find out the number of records and save it to n
+n=len(x_train)
 print("Amount of datas:",n)
+
+# we retrieve the k that we want within input
 k=input("Enter k: ")
 k=int(k)
-part=int(n/10)
-part2=0
+
+# because we're using 10-folds cross validation to generate a more distributed train,
+# we first determine the number of records each fold has
+fold_num=int(n/10)
+
+# if our data isn't divisible by 10, then we put the rest to rest_data
+# example, if we have 500 records (divisible by 10), then:
+# 50 50 50 50 50 50 50 50 50 50
+# but if we have 524, then:
+# 53 53 53 53 52 52 52 52 52 52.
+rest_data=0
 if n%10!=0:
-	part2=n%10
-result=[]
+	rest_data=n%10
+
+# y_test will later hold the result of our predictions to be compared to y_train
+y_test=[]
+
+# for the distance measurements, we'll use distance form scipy.spatial
 from scipy.spatial import distance
-for x in range(10-part2):
+
+# rest_data contains the value of modulus 10, then in the first n-rest_data,
+# we're gonna add an addition iteration of n/10 (fold_num)
+for x in range(10-rest_data):
 	print("Loading iteration...", x+1)
-	for y in range(x*part,part*(x+1)):
+	for y in range(x*fold_num,fold_num*(x+1)):
 		arr=[]
-		for z in range(x*part):
-			a=csv[y]
-			b=csv[z]
+		for z in range(x*fold_num):
+			a=x_train[y]
+			b=x_train[z]
 			dst=distance.cosine(a,b)
 			arr.append(dst)
-		for z in range(x*part,part*(x+1)):
+		for z in range(x*fold_num,fold_num*(x+1)):
 			inf=math.inf
 			arr.append(inf)
-		for z in range(part*(x+1),n):
-			a=csv[y]
-			b=csv[z]
+		for z in range(fold_num*(x+1),n):
+			a=x_train[y]
+			b=x_train[z]
 			dst=distance.cosine(a,b)
 			arr.append(dst)
 		knn=np.array(arr)
@@ -34,31 +58,31 @@ for x in range(10-part2):
 		zero=0
 		one=0
 		for i in range(k):
-			if csv2[knn[i]]==0:
+			if y_train[knn[i]]==0:
 				zero+=1
 			else:
 				one+=1
 		if one==zero:
-			result.append(csv2[knn[0]])
+			y_test.append(y_train[knn[0]])
 		elif one>zero:
-			result.append(1)
+			y_test.append(1)
 		else:
-			result.append(0)
-for x in range(10-part2,10):
+			y_test.append(0)
+for x in range(10-rest_data,10):
 	print("Loading iteration...", x+1)
-	for y in range(x*part,part*(x+1)+1):
+	for y in range(x*fold_num,fold_num*(x+1)+1):
 		arr=[]
-		for z in range(x*part):
-			a=csv[y]
-			b=csv[z]
+		for z in range(x*fold_num):
+			a=x_train[y]
+			b=x_train[z]
 			dst=distance.cosine(a,b)
 			arr.append(dst)
-		for z in range(x*part,part*(x+1)+1):
+		for z in range(x*fold_num,fold_num*(x+1)+1):
 			inf=math.inf
 			arr.append(inf)
-		for z in range(part*(x+1)+1,n):
-			a=csv[y]
-			b=csv[z]
+		for z in range(fold_num*(x+1)+1,n):
+			a=x_train[y]
+			b=x_train[z]
 			dst=distance.cosine(a,b)
 			arr.append(dst)
 		knn=np.array(arr)
@@ -66,20 +90,20 @@ for x in range(10-part2,10):
 		zero=0
 		one=0
 		for i in range(k):
-			if csv2[knn[i]]==0:
+			if y_train[knn[i]]==0:
 				zero+=1
 			else:
 				one+=1
 		if one==zero:
-			result.append(csv2[knn[0]])
+			y_test.append(y_train[knn[0]])
 		elif one>zero:
-			result.append(1)
+			y_test.append(1)
 		else:
-			result.append(0)
+			y_test.append(0)
 print("Iterations completed")
 counter=0
 for x in range(n):
-	if result[x]==csv2[x]:
+	if y_test[x]==y_train[x]:
 		counter+=1
 acc=100*counter/n
 print("Correctly Classified Instances:",counter)
